c26d1961419c077f5ec345b9da208256f7d3f357
eXhumer <exhumer1@protonmail.com>
2023-02-26 23:22:04 -0700

    feat: Add new methods * Added method for posting messages * Added methods for creating DM channels

--- exdc/_client/_rest.py
+++ exdc/_client/_rest.py
@@ -1,11 +1,19 @@
+from json import dumps
 from logging import getLogger
+from mimetypes import guess_type
+from pathlib import Path
+from random import randint
+from typing import IO
 
 from httpx import Client
 
 from .._consts import __user_agent__
 from ..exception import RESTException
-from ..type.interactions.application_command import ApplicationCommand
+from ..type.channel import AllowedMentions, Attachment, Channel, Embed, MessageFlag, \
+    MessageReference
 from ..type.interactions import Interaction, InteractionResponse
+from ..type.interactions.application_command import ApplicationCommand
+from ..type.interactions.message_component import ActionRowComponent
 from ..type.rest import GetGatewayResponse
 
 try:
@@ -19,83 +27,67 @@ except ImportError:
 class REST:
     API_URL = "https://discord.com/api"
     VERSION = 10
+    __CLIENT = None
     __LOGGER = getLogger("exdc.REST")
 
-    def __init__(self, authorization: str | None = None, http_client: Client | None = None,
-                 user_agent: str | None = None):
-        self.__http_client = REST.__setup_http_client(authorization=authorization,
-                                                      http_client=http_client,
-                                                      user_agent=user_agent)
+    def __init__(self, authorization: str | None = None, user_agent: str | None = None):
+        if REST.__CLIENT is None:
+            REST.__CLIENT = Client(http2=h2_available)
+            REST.__CLIENT.base_url = f"{REST.API_URL}/v{REST.VERSION}/"
+            REST.__CLIENT.follow_redirects = True
+            REST.__CLIENT.headers["User-Agent"] = __user_agent__
 
-    @staticmethod
-    def __setup_http_client(authorization: str | None = None, http_client: Client | None = None,
-                            user_agent: str | None = None):
-        if not http_client:
-            http_client = Client(http2=h2_available)
+        self.__authorization = authorization
+        self.__user_agent = user_agent
 
-        http_client.base_url = f"{REST.API_URL}/v{REST.VERSION}/"
-        http_client.follow_redirects = True
-        http_client.headers["User-Agent"] = user_agent or __user_agent__
+    def _request(self, method: str, url: str, **kwargs):
+        if self.__user_agent:
+            if "headers" in kwargs:
+                kwargs["headers"] |= {"User-Agent": self.__user_agent}
 
-        if authorization:
-            http_client.headers["Authorization"] = authorization
+            else:
+                kwargs["headers"] = {"User-Agent": self.__user_agent}
 
-        return http_client
+        if self.__authorization:
+            if "headers" in kwargs:
+                kwargs["headers"] |= {"Authorization": self.__authorization}
+
+            else:
+                kwargs["headers"] = {"Authorization": self.__authorization}
+
+        res = REST.__CLIENT.request(method, url, **kwargs)
+
+        if res.status_code >= 400:
+            raise RESTException(res)
+
+        return res
 
     def add_global_command(self, application_id: str, command: ApplicationCommand):
-        res = self.__http_client.post(f"applications/{application_id}/commands", json=command)
-
-        if not res.is_success:
-            print(res.status_code)
-            print(res.text)
-            raise RESTException(res)
-
-        return res
+        return self._request("POST", f"applications/{application_id}/commands", json=command)
 
     def add_guild_command(self, application_id: str, guild_id: str, command: ApplicationCommand):
-        res = self.__http_client.post(f"applications/{application_id}/guilds/{guild_id}/commands",
-                                      json=command)
+        return self._request("POST", f"applications/{application_id}/guilds/{guild_id}/commands",
+                             json=command)
 
-        if not res.is_success:
-            raise RESTException(res)
-
-        return res
+    def create_dm_channel(self, recipient_id: str):
+        res = self._request("POST", "users/@me/channels", json={"recipient_id": recipient_id})
+        channel: Channel = res.json()
+        return channel
 
     def delete_global_command(self, application_id: str, command_id: str):
-        res = self.__http_client.delete(f"applications/{application_id}/commands/{command_id}")
-
-        if not res.is_success:
-            raise RESTException(res)
-
-        return res
+        return self._request("DELETE", f"applications/{application_id}/commands/{command_id}")
 
     def delete_guild_command(self, application_id: str, guild_id: str, command_id: str):
-        res = self.__http_client.delete(f"applications/{application_id}/guilds/{guild_id}/" +
-                                        f"commands/{command_id}")
+        return self._request("DELETE", f"applications/{application_id}/guilds/{guild_id}/" +
+                             f"commands/{command_id}")
 
-        if not res.is_success:
-            raise RESTException(res)
-
-        return res
-
-    @staticmethod
-    def get_gateway(http_client: Client | None = None, user_agent: str | None = None):
-        http_client = REST.__setup_http_client(http_client=http_client, user_agent=user_agent)
-        res = http_client.get("gateway")
-
-        if not res.is_success:
-            raise RESTException(res)
-
+    def get_gateway(self):
+        res = self._request("GET", "gateway")
         data: GetGatewayResponse = res.json()
         return data
 
     def get_global_command(self, application_id: str, command_id: str):
-        res = self.__http_client.get(f"applications/{application_id}/commands/{command_id}")
-
-        if not res.is_success:
-            raise RESTException(res)
-
-        return res
+        return self._request("GET", f"applications/{application_id}/commands/{command_id}")
 
     def get_global_commands(self, application_id: str, with_localizations: bool | None = None):
         if with_localizations is not None:
@@ -104,19 +96,13 @@ class REST:
         else:
             params = None
 
-        res = self.__http_client.get(f"applications/{application_id}/commands", params=params)
-
-        if not res.is_success:
-            raise RESTException(res)
+        res = self._request("GET", f"applications/{application_id}/commands", params=params)
 
         return res
 
     def get_guild_command(self, application_id: str, guild_id: str, command_id: str):
-        res = self.__http_client.get(f"applications/{application_id}/guilds/{guild_id}/" +
-                                     f"commands/{command_id}")
-
-        if not res.is_success:
-            raise RESTException(res)
+        res = self._request("GET", f"applications/{application_id}/guilds/{guild_id}/" +
+                            f"commands/{command_id}")
 
         return res
 
@@ -128,44 +114,177 @@ class REST:
         else:
             params = None
 
-        res = self.__http_client.get(f"applications/{application_id}/guilds/{guild_id}/commands",
-                                     params=params)
-
-        if not res.is_success:
-            raise RESTException(res)
+        res = self._request("GET", f"applications/{application_id}/guilds/{guild_id}/commands",
+                            params=params)
 
         return res
 
     def interaction_response(self, interaction: Interaction, callback: InteractionResponse):
-        res = self.__http_client.post(
-            f"interactions/{interaction['id']}/{interaction['token']}/callback", json=callback)
+        res = self._request("POST",
+                            f"interactions/{interaction['id']}/{interaction['token']}/callback",
+                            json=callback)
 
-        if not res.is_success:
+        return res
+
+    def post_message(self, channel_id: str, content: str | None = None, tts: bool | None = None,
+                     embeds: list[Embed] | None = None,
+                     allowed_mentions: AllowedMentions | None = None,
+                     message_reference: MessageReference | None = None,
+                     components: list[ActionRowComponent] | None = None,
+                     sticker_ids: list[str] | None = None, flags: MessageFlag | None = None,
+                     attachments: list[Attachment] | None = None,
+                     uploads: list[tuple[IO[bytes], str, str | None]] | None = None):
+        assert content or embeds or sticker_ids or components or uploads
+
+        payload = {}
+
+        if tts:
+            payload |= {"tts": tts}
+
+        if content:
+            payload |= {"content": content}
+
+        if sticker_ids:
+            payload |= {"sticker_ids": sticker_ids}
+
+        if allowed_mentions:
+            payload |= {"allowed_mentions": allowed_mentions}
+
+        if message_reference:
+            payload |= {"message_reference": message_reference}
+
+        if components:
+            payload |= {"components": components}
+
+        if flags:
+            payload |= {"flags": flags}
+
+        if attachments:
+            payload |= {"attachments": attachments}
+
+        if uploads:
+            files = {
+                "payload_json": (None, dumps(payload, separators=(",", ":")), "application/json"),
+            }
+
+            for stream, filename, attachment_id in uploads:
+                attachment_id = attachment_id or REST.random_attachment_id()
+                mimetype = guess_type(filename, strict=False)[0]
+                assert mimetype is not None and mimetype.startswith(("image/", "video/"))
+
+                files |= {
+                    f"files[{attachment_id}]": (f"{attachment_id}{Path(filename).suffix}",
+                                                stream, mimetype),
+                }
+
+            json = None
+
+        else:
+            files = None
+            json = payload
+
+        res = self._request("POST", f"channels/{channel_id}/messages", files=files, json=json)
+
+        return res
+
+    @staticmethod
+    def post_webhook_message(webhook_id: str, webhook_token: str, content: str | None = None,
+                             username: str | None = None, avatar_url: str | None = None,
+                             tts: bool | None = None, embeds: list[Embed] | None = None,
+                             allowed_mentions: AllowedMentions | None = None,
+                             components: list[ActionRowComponent] | None = None,
+                             flags: MessageFlag | None = None,
+                             attachments: list[Attachment] | None = None,
+                             uploads: list[tuple[IO[bytes], str, str | None]] | None = None,
+                             wait: bool | None = None, thread_id: str | None = None):
+        assert content or embeds or components or uploads
+
+        payload = {}
+
+        if username:
+            payload |= {"username": username}
+
+        if avatar_url:
+            payload |= {"avatar_url": avatar_url}
+
+        if tts:
+            payload |= {"tts": tts}
+
+        if content:
+            payload |= {"content": content}
+
+        if allowed_mentions:
+            payload |= {"allowed_mentions": allowed_mentions}
+
+        if components:
+            payload |= {"components": components}
+
+        if flags:
+            payload |= {"flags": flags}
+
+        if attachments:
+            payload |= {"attachments": attachments}
+
+        if uploads:
+            files = {
+                "payload_json": (None, dumps(payload, separators=(",", ":")),
+                                 "application/json"),
+            }
+
+            for stream, filename, attachment_id in uploads:
+                attachment_id = attachment_id or REST.random_attachment_id()
+                mimetype = guess_type(filename, strict=False)[0]
+                assert mimetype is not None and mimetype.startswith(("image/", "video/"))
+
+                files |= {
+                    f"files[{attachment_id}]": (f"{attachment_id}{Path(filename).suffix}",
+                                                stream, mimetype),
+                }
+
+            json = None
+
+        else:
+            files = None
+            json = payload
+
+        if wait is not None or thread_id is not None:
+            params = {}
+
+            if wait is not None:
+                params |= {"wait": wait}
+
+            if thread_id is not None:
+                params |= {"thread_id": thread_id}
+
+        else:
+            params = None
+
+        res = REST.__CLIENT.post(f"webhooks/{webhook_id}/{webhook_token}", files=files, json=json,
+                                 params=params)
+
+        if res.status_code >= 400:
             raise RESTException(res)
 
         return res
 
+    @staticmethod
+    def random_attachment_id():
+        return str(randint(0, 0x7fffffffffffffff))
+
     def update_global_command(self, application_id: str, command_id: str,
                               command: ApplicationCommand):
-        res = self.__http_client.patch(f"applications/{application_id}/commands/{command_id}",
-                                       json=command)
-
-        if not res.is_success:
-            raise RESTException(res)
+        res = self._request("PATCH", f"applications/{application_id}/commands/{command_id}",
+                            json=command)
 
         return res
 
     def update_guild_command(self, application_id: str, guild_id: str, command_id: str,
                              command: ApplicationCommand):
-        res = self.__http_client.patch(f"applications/{application_id}/guilds/{guild_id}/" +
-                                       f"commands/{command_id}", json=command)
-
-        if not res.is_success:
-            raise RESTException(res)
+        res = self._request("PATCH", f"applications/{application_id}/guilds/{guild_id}/" +
+                            f"commands/{command_id}", json=command)
 
         return res
 
     @classmethod
-    def with_bot_token(cls, token: str, http_client: Client | None = None,
-                       user_agent: str | None = None):
-        return cls(f"Bot {token}", http_client=http_client, user_agent=user_agent)
+    def with_bot_token(cls, token: str, user_agent: str | None = None):
+        return cls(f"Bot {token}", user_agent=user_agent)
